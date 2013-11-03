06603b53fadf65fd473427b6bcb914be27ea84c8
aavaz <aavaz12@gmail.com>
2013-11-03 12:42:26 +1100

    Ran bundle install

---
+++ vendor/bundle/gems/pg-0.17.0/ext/siteconf20131103-21999-3nyzym.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131103-21999-1zqqs8"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
